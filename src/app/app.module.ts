e87887b7126201b68ece73d73e7e958958e88bd3
Duduman Bogdan <dudumanbogdan@gmail.com>
2018-04-06 20:16:45 +0300

    Add material design changes; add test material button works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
